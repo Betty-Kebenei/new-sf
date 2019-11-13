7d9a8493c0f9c11f5b19d3168727074a636b7340
Betty-Kebenei <kebeneibetty13@gmail.com>
2019-11-13 14:53:24 +0300

    add a header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../../public/scss/global.scss';
 
+import Header from './header/header';
 import About from './About';
 import DirectionDeterminants from './DirectionDeterminants';
 import Activities from './activities/Activities';
@@ -8,6 +9,7 @@ import Activities from './activities/Activities';
 const App = () => {
   return (
     <div className="hey">
+      <Header />
       <About />
       <DirectionDeterminants />
       <Activities />
