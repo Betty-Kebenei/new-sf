0759885bd66566a89aa6262ee8254b270f2d07df
Betty-Kebenei <kebeneibetty13@gmail.com>
2020-01-21 17:06:44 +0300

    replace typing with a carousel for the quotes

--- src/components/quotes/Quotes.jsx
+++ src/components/quotes/Quotes.jsx
@@ -1,15 +1,32 @@
 import React from 'react';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { Carousel } from 'react-responsive-carousel';
 import allQuotes from './QuotesArray';
 import sendMePic from '../../images/send-me.jpg';
 
 const Quotes = () => {
   return (
-    <div className="quotes">
-      <img src={sendMePic} alt="send me" />
-      <div className="quotes-section">
-        <p>{allQuotes}</p>
+    <Carousel
+      className="quotes"
+      showThumbs={false}
+      showArrows={false}
+      useKeyboardArrows
+      infiniteLoop
+      autoPlay
+    >
+      <div>
+        <img src={sendMePic} alt="send me" />
+        <div className="quotes-section">
+          <p>{allQuotes[0]}</p>
+        </div>
       </div>
-    </div>
+      <div>
+        <img src={sendMePic} alt="send me" />
+        <div className="quotes-section">
+          <p>{allQuotes[1]}</p>
+        </div>
+      </div>
+    </Carousel>
   );
 };
 export default Quotes;
