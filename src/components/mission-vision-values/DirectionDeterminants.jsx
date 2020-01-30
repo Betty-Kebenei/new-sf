3679448768e5d7ed223c28c3407de0c89929eb91
Betty-Kebenei <kebeneibetty13@gmail.com>
2020-01-30 12:44:00 +0300

    update direction determinants content

--- src/components/mission-vision-values/DirectionDeterminants.jsx
+++ src/components/mission-vision-values/DirectionDeterminants.jsx
@@ -2,17 +2,23 @@ import React from 'react';
 import missionIcon from '../../images/mission.png';
 import visionIcon from '../../images/vision.png';
 import valuesIcon from '../../images/values.png';
+import verseIcon from '../../images/verse.png';
+import objectivesIcon from '../../images/objectives.png';
 
 const DirectionDeterminants = () => {
   const mission =
-    'Pellentesque habitant morbi tristique senectus et netus et malesuada' +
-    'fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae,' +
-    'ultricies eget, tempor sit amet, ante. Donec eu libero sit amet ' +
-    'quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat ' +
-    'eleifend leo.';
+    'Raise resources and mobilize believers in the marketplace to ' +
+    'take part in froniter missions.';
   const vision =
-    'Pellentesque habitant morbi tristique senectus et netus et malesuada' +
-    ' fames ac turpis egestas.';
+    'To raise resources for Kingdom advancement and provide moral and ' +
+    'prayer support to those who go.';
+  const verse =
+    'How, then, can they call on the one they have not believed in?' +
+    'And how can they believe in the one of whom they have not heard?' +
+    'And how can they hear without someone preaching to them?' +
+    'And how can anyone preach unless they are sent?' +
+    'As it is written: “How beautiful are the feet of those who bring ' +
+    'good news!” ~ Romans 10:14-15 (NIV)';
   return (
     <div className="direction-determinants" id="mission">
       <div className="direction-determinants-mission">
@@ -25,13 +31,29 @@ const DirectionDeterminants = () => {
         <h2>OUR VISION</h2>
         <p>{vision}</p>
       </div>
+      <div className="direction-determinants-verse">
+        <img src={verseIcon} alt="vision" />
+        <h2>OUR THEME VERSE</h2>
+        <p>{verse}</p>
+      </div>
       <div className="direction-determinants-values">
-        <img src={valuesIcon} alt="values" />
+        <img src={valuesIcon} alt="verse" />
         <h2>OUR CORE VALUES</h2>
         <ul>
-          <li>i. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
-          <li>ii. Aliquam tincidunt mauris eu risus.</li>
-          <li>iii. Vestibulum auctor dapibus neque.</li>
+          <li>i. Faith in Jesus Christ.</li>
+          <li>ii. Commitment.</li>
+          <li>iii. Passion.</li>
+        </ul>
+      </div>
+      <div className="direction-determinants-objectives">
+        <img src={objectivesIcon} alt="objectives" />
+        <h2>OUR OBJECTIVES</h2>
+        <ul>
+          <li>i. Be accountable to other people called to the marketplace.</li>
+          <li>ii. Raise resources for kingdom advancement.</li>
+          <li>iii. Provide moral support for those who go.</li>
+          <li>iv. Encourage one another.</li>
+          <li>v. Brainstorm ideas.</li>
         </ul>
       </div>
     </div>
