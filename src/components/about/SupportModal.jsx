d41fef91852b45c97c50c8d545807a5a1dd01743
Betty-Kebenei <kebeneibetty13@gmail.com>
2020-03-03 12:33:05 +0300

    refactor support modal component

--- src/components/about/SupportModal.jsx
+++ src/components/about/SupportModal.jsx
@@ -3,7 +3,7 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React, { useState } from 'react';
+import React from 'react';
 
 const SupportModal = ({ openAndCloseSupportModal }) => {
   return (
