b3db4ae80f71722d7d2a100bb5bfd7c18c606512
Betty-Kebenei <kebeneibetty13@gmail.com>
2020-03-03 12:31:35 +0300

    refactor activities component

--- src/components/activities/Activities.jsx
+++ src/components/activities/Activities.jsx
@@ -1,121 +1,30 @@
 import React from 'react';
+import allActivities from './ActivitiesArray';
 
 const Activities = () => {
+  const renderActivities = allActivities.map(activity => {
+    return (
+      <div className="activity-card" key={activity.title}>
+        <div className="activity-card-inner">
+          <div className="activity-card-front">
+            <img src={activity.imageUrl} alt="Avatar" />
+          </div>
+          <div className="activity-card-back">
+            <h2>{activity.title}</h2>
+            <p>{activity.description}</p>
+            <p>{activity.quote}</p>
+          </div>
+        </div>
+      </div>
+    );
+  });
+
   return (
     <div className="activities" id="activities">
       <div className="activities-title">
         <h2>ACTIVITIES</h2>
       </div>
-      <div className="activities-body">
-        <div className="activity-card">
-          <div className="activity-card-inner">
-            <div className="activity-card-front">
-              <img src="https://res.cloudinary.com/dkp2ocmaw/image/upload/v1580564407/meeting-pic_v6tf9y.jpg" alt="Avatar" />
-            </div>
-            <div className="activity-card-back">
-              <h2>Monthly meetings</h2>
-              <p>Meeting monthly for fellowships</p>
-              <p>
-                "The mark of a great church is not its seating capacity,
-                but its sending capacity." ~ Mike Stachura
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="activity-card">
-          <div className="activity-card-inner">
-            <div className="activity-card-front">
-              <img src="https://res.cloudinary.com/dkp2ocmaw/image/upload/v1580564385/financial-pic_lspzuy.jpg" alt="Avatar" />
-            </div>
-            <div className="activity-card-back">
-              <h2>Monthly financially contributions</h2>
-              <p>Contributing monthly for support</p>
-              <p>
-                "You can give without loving. But you cannot love without
-                giving." ~ Amy Carmichael
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="activity-card">
-          <div className="activity-card-inner">
-            <div className="activity-card-front">
-              <img src="https://res.cloudinary.com/dkp2ocmaw/image/upload/v1580564394/logistics-pic_soqfkd.jpg" alt="Avatar" />
-            </div>
-            <div className="activity-card-back">
-              <h2>Missionary logistics support</h2>
-              <p>Helping out with the logistics</p>
-              <p>
-                "We must be global Christians with a global vision because our
-                God is a global God." ~ John Stott
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="activity-card">
-          <div className="activity-card-inner">
-            <div className="activity-card-front">
-              <img src="https://res.cloudinary.com/dkp2ocmaw/image/upload/v1580564406/moral-pic_pr30vd.jpg" alt="Avatar" />
-            </div>
-            <div className="activity-card-back">
-              <h2>Missionary moral support</h2>
-              <p>Listening to them</p>
-              <p>
-                "Let my heart be broken with the things that break God's heart."
-                ~ Bob Pierce
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="activity-card">
-          <div className="activity-card-inner">
-            <div className="activity-card-front">
-              <img src="https://res.cloudinary.com/dkp2ocmaw/image/upload/v1580564410/prayer-pic_mydcom.jpg" alt="Avatar" />
-            </div>
-            <div className="activity-card-back">
-              <h2>Weekly prayer and fasting</h2>
-              <p>Praying weekly for a specific missionary</p>
-              <p>
-                "Prayer is the mighty engine that is to move the missionary
-                work." ~ A.B. Simpson
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="activity-card">
-          <div className="activity-card-inner">
-            <div className="activity-card-front">
-              <img src="https://res.cloudinary.com/dkp2ocmaw/image/upload/v1580564405/re-entry-pic_e2ii7f.jpg" alt="Avatar" />
-            </div>
-            <div className="activity-card-back">
-              <h2>Missionary re-entry support</h2>
-              <p>
-                Praticipating in finding a place for the missionaries to stay
-                during their furlough.
-              </p>
-              <p>
-                "If Jesus Christ be God and died for me, then no sacrifice can
-                be too great for me to make for Him." ~ C.T. Studd
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="activity-card">
-          <div className="activity-card-inner">
-            <div className="activity-card-front">
-              <img src="https://res.cloudinary.com/dkp2ocmaw/image/upload/v1580564418/visits-pic_sqdp8r.jpg" alt="Avatar" />
-            </div>
-            <div className="activity-card-back">
-              <h2>Missionary visits</h2>
-              <p>Going to visit frontier missioneries on the mission field</p>
-              <p>
-                "Love is the root of missions; sacrifice is the fruit of
-                missions." ~ Roderick Davis
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
+      <div className="activities-body">{renderActivities}</div>
     </div>
   );
 };
